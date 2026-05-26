000503156d2f0eb21e764b5136ee9f03b3095c6c
rohan <rohanuddin066@gmail.com>
2026-05-26 15:47:53 +0500

    muscle gain window added

--- main project.py
+++ main project.py
@@ -19,9 +19,10 @@ conn.commit()
 conn.close()
 
 def login():
-    global email_entry,password_entry,currunt_height,currunt_weight,currunt_name
+    global email_entry,password_entry,currunt_height,currunt_weight,currunt_name,currunt_age,age_entry
     e=email_entry.get()
     p=password_entry.get()
+    currunt_age=age_entry.get()
     conn=sqlite3.connect("user.db")
     cursor=conn.cursor()
 
@@ -149,11 +150,11 @@ def sign_up_window():
 
 
 def main_window():
-    global name_entry,currunt_height,currunt_weight,currunt_name,age_entry
+    global name_entry,currunt_height,currunt_weight,currunt_name,age_entry,currunt_age
     n=currunt_name
     h=currunt_height
     w=currunt_weight
-    ag=age_entry.get()
+    ag=currunt_age
     for widgets in root.winfo_children():
         widgets.destroy()
 
@@ -165,38 +166,36 @@ def main_window():
     out.pack(side=tk.RIGHT,padx=5)
     feed=tk.Button(f,text="Feedback",font="arial 10",bg="light gray",fg="black",width=10,pady=5,command=feedback_window)
     feed.pack(side=tk.RIGHT,padx=5)
-    f1=tk.Frame(root,width=700,height=300,bg="white",borderwidth=3,relief="groove")
+    tk.Label(f,text="Fitness App",font="arial 15 bold",bg="light gray",fg="black").pack(side=tk.LEFT,padx=10)
+    f1=tk.Frame(root,width=700,height=300,bg="white")
     f1.pack(fill=tk.BOTH)
-    tk.Label(f1,text=f"hey {n}",font="arial 20 bold",bg="white",fg="black",width=24,pady=7).pack(pady=10)
+    tk.Label(f1,text=f"hey {n}",font="arial 20 bold",bg="white",fg="black",width=10,pady=7).pack(pady=10,side=tk.TOP)
 
-    f2=tk.Frame(f1,width=100,height=30,bg="light gray")
+    f2=tk.Frame(f1,width=100,height=30,bg="gray95")
     f2.pack(pady=7,side=tk.LEFT,padx=10)
-    tk.Label(f2,text="Height:",font="arial 8",bg="light gray",fg="black",width=70,pady=5).grid(row=0,column=0)
-    tk.Label(f2,text=f"{h} cm",font="arial 15",bg="light gray",fg="black").grid(row=1,column=0)
+    tk.Label(f2,text="Height:",font="arial 8",bg="gray95",fg="black",width=70,pady=5).grid(row=0,column=0)
+    tk.Label(f2,text=f"{h} cm",font="arial 15",bg="gray95",fg="black").grid(row=1,column=0)
 
-    f3=tk.Frame(f1,width=100,height=30,bg="light gray")
-    f3.pack(pady=7,side=tk.LEFT,padx=20)
-    tk.Label(f3,text="Weight:",font="arial 8",bg="light gray",fg="black",width=70,pady=5).grid(row=0,column=0)
-    tk.Label(f3,text=f"{w} kg",font="arial 15",bg="light gray",fg="black").grid(row=1,column=0)
-    
-    f4=tk.Frame(root,width=100,height=30,bg="white",borderwidth=3,relief="groove")
+    f3=tk.Frame(f1,width=100,height=30,bg="gray95")
+    f3.pack(pady=7,side=tk.LEFT,padx=18)
+    tk.Label(f3,text="Weight:",font="arial 8",bg="gray95",fg="black",width=70,pady=5).grid(row=0,column=0)
+    tk.Label(f3,text=f"{w} kg",font="arial 15",bg="gray95",fg="black").grid(row=1,column=0)
+
+    f4=tk.Frame(root,width=100,height=30,bg="white")
     f4.pack(fill=tk.BOTH)
-    f5=tk.Frame(f4,width=100,height=30,bg="light gray")
+    f5=tk.Frame(f4,width=100,height=30,bg="gray95")
     f5.pack(pady=7,padx=10)
-    tk.Label(f5,text="Age:",font="arial 8",bg="light gray",fg="black",width=170,pady=5,justify="center").grid(row=0,column=0)
-    tk.Label(f5,text=f"{ag}",font="arial 15",bg="light gray",fg="black").grid(row=1,column=0)
+    tk.Label(f5,text="Age:",font="arial 8",bg="gray95",fg="black",width=170,pady=5).grid(row=0,column=0)
+    tk.Label(f5,text=f"{ag} yrs",font="arial 15",bg="gray95",fg="black").grid(row=1,column=0)
 
     #add scrapped data here
 
-    f6=tk.Frame(root,width=100,height=50,bg="white",borderwidth=3,relief="groove")
+    f6=tk.Frame(root,width=100,height=50,bg="white")
     f6.pack(fill=tk.BOTH,side=tk.BOTTOM)
-    fat_lose=tk.Button(f6,text="Fat Loss",font="arial 10",bg="light blue",fg="dark blue",width=50,pady=50,command=fat_loss_window)
+    fat_lose=tk.Button(f6,text="Fat Loss\n Cardio & burn exersices",font="arial 13",bg="lightblue1",fg="blue4",width=40,pady=50,padx=10,command=fat_loss_window,anchor="w",justify="left")
     fat_lose.pack(pady=10,side=tk.LEFT,padx=30)
-    muscle_gain=tk.Button(f6,text="Muscle Gain",font="arial 10",bg="light green",fg="dark green",width=50,pady=50,command=muscle_gain_window)
+    muscle_gain=tk.Button(f6,text="Muscle Gain\n Strength & resistance exercises",font="arial 13",bg="aquamarine1",fg="dark green",width=50,pady=50,padx=10,command=muscle_gain_window,anchor="w",justify="left")
     muscle_gain.pack(pady=10,side=tk.LEFT,padx=30)
-
-
-    
 
 
 def feedback_window():
@@ -206,7 +205,101 @@ def fat_loss_window():
     pass
 
 def muscle_gain_window():
-    pass
+    for widgets in root.winfo_children():
+        widgets.destroy()
+    root.title("select muscle window")
+    root.maxsize(900, 600)
+    f=tk.Frame(root,width=400,height=40,bg="white",pady=20)
+    f.pack(fill=tk.BOTH,side=tk.TOP)
+    back_btn=tk.Button(f,text="<-- back to main menu",font="arial 10 bold",bg="dark blue",fg="white",command=main_window,pady=10)
+    back_btn.pack(side=tk.LEFT,padx=10)
+    tk.Label(f,text="Strength Training",font="arial 15",bg="white",fg="black").pack(side=tk.LEFT,padx=10)
+    f1=tk.Frame(root,width=700,height=300,bg="white")
+    f1.pack(fill=tk.BOTH,expand=True)
+    title_frame=tk.Frame(f1,width=100,height=100,bg="lightblue1",borderwidth=2,pady=40)
+    title_frame.pack(side=tk.TOP,fill=tk.BOTH)
+    #add icon here 
+    tk.Label(title_frame,text="Choose your muscle group\nSelect a muscle - we'll show you the best exercises for it!",font="arial 13",bg="lightblue1",fg="blue4",justify="left").pack(pady=5,padx=30,anchor="nw")
+    main_frame=tk.Frame(f1,width=700,height=500,bg="white")
+    main_frame.pack(side=tk.TOP,fill=tk.BOTH,expand=True,padx=20,pady=15)
+    tk.Label(main_frame,text="Select Muscle Group",font="arial 11 bold",bg="white",fg="black").grid(row=0,column=0,pady=10,padx=20,sticky="w")
+
+    chest_btn=tk.Button(main_frame,text="chest",font="arial 10",bg="light gray",fg="black",width=30,pady=40,command=chest_window)
+    chest_btn.grid(row=1,column=0,pady=15,padx=22)
+
+    chest_btn=tk.Button(main_frame,text="Abs",font="arial 10",bg="light gray",fg="black",width=30,pady=40,command=abs_window)
+    chest_btn.grid(row=1,column=1,pady=15,padx=22)
+
+    chest_btn=tk.Button(main_frame,text="Back",font="arial 10",bg="light gray",fg="black",width=30,pady=40,command=back_window)
+    chest_btn.grid(row=1,column=2,pady=15,padx=22)
+
+    chest_btn=tk.Button(main_frame,text="Arms",font="arial 10",bg="light gray",fg="black",width=30,pady=40,command=arms_window)
+    chest_btn.grid(row=2,column=0,pady=15,padx=22)
+
+    chest_btn=tk.Button(main_frame,text="Shoulder",font="arial 10",bg="light gray",fg="black",width=30,pady=40,command=shoulder_window)
+    chest_btn.grid(row=2,column=1,pady=15,padx=22)
+
+    chest_btn=tk.Button(main_frame,text="Legs",font="arial 10",bg="light gray",fg="black",width=30,pady=40,command=legs_window)
+    chest_btn.grid(row=2,column=2,pady=15,padx=22)
+
+
+
+
+def chest_window():
+    for widgets in root.winfo_children():
+        widgets.destroy()
+    root.title("Chest muscle")
+    root.maxsize(900, 600)
+    f=tk.Frame(root,width=400,bg="white",pady=13)
+    f.pack(fill=tk.BOTH,side=tk.TOP)
+    back_btn=tk.Button(f,text="<-- back to Select Muscles",font="arial 10 bold",bg="dark blue",fg="white",command=muscle_gain_window,pady=10)
+    back_btn.pack(side=tk.LEFT,padx=10)
+def back_window():
+    for widgets in root.winfo_children():
+        widgets.destroy()
+    root.title("Back muscle")
+    root.maxsize(900, 600)
+    f=tk.Frame(root,width=400,bg="white",pady=13)
+    f.pack(fill=tk.BOTH,side=tk.TOP)
+    back_btn=tk.Button(f,text="<-- back to Select Muscles",font="arial 10 bold",bg="dark blue",fg="white",command=muscle_gain_window,pady=10)
+    back_btn.pack(side=tk.LEFT,padx=10)
+def abs_window():
+    for widgets in root.winfo_children():
+        widgets.destroy()
+    root.title("Abs muscle")
+    root.maxsize(900, 600)
+    f=tk.Frame(root,width=400,bg="white",pady=13)
+    f.pack(fill=tk.BOTH,side=tk.TOP)
+    back_btn=tk.Button(f,text="<-- back to Select Muscles",font="arial 10 bold",bg="dark blue",fg="white",command=muscle_gain_window,pady=10)
+    back_btn.pack(side=tk.LEFT,padx=10)
+def arms_window():
+    for widgets in root.winfo_children():
+        widgets.destroy()
+    root.title("Arms muscle")
+    root.maxsize(900, 600)
+    f=tk.Frame(root,width=400,bg="white",pady=13)
+    f.pack(fill=tk.BOTH,side=tk.TOP)
+    back_btn=tk.Button(f,text="<-- back to Select Muscles",font="arial 10 bold",bg="dark blue",fg="white",command=muscle_gain_window,pady=10)
+    back_btn.pack(side=tk.LEFT,padx=10)
+def shoulder_window():
+    for widgets in root.winfo_children():
+        widgets.destroy()
+    root.title("Shoulder muscle")
+    root.maxsize(900, 600)
+    f=tk.Frame(root,width=400,bg="white",pady=13)
+    f.pack(fill=tk.BOTH,side=tk.TOP)
+    back_btn=tk.Button(f,text="<-- back to Select Muscles",font="arial 10 bold",bg="dark blue",fg="white",command=muscle_gain_window,pady=10)
+    back_btn.pack(side=tk.LEFT,padx=10)
+def legs_window():
+    for widgets in root.winfo_children():
+        widgets.destroy()
+    root.title("Legs muscle")
+    root.maxsize(900, 600)
+    f=tk.Frame(root,width=400,bg="white",pady=13)
+    f.pack(fill=tk.BOTH,side=tk.TOP)
+    back_btn=tk.Button(f,text="<-- back to Select Muscles",font="arial 10 bold",bg="dark blue",fg="white",command=muscle_gain_window,pady=10)
+    back_btn.pack(side=tk.LEFT,padx=10)
+    
 
 
 
